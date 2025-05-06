7167672ed6e235addb7cc304b7521558a3ffe70e
Azhar Siddiqui <azhar.sid@icloud.com>
2025-05-07 00:52:57 +0530

    Adding the changes in exception.py

--- src/logger.py
+++ src/logger.py
@@ -2,10 +2,9 @@ import logging
 import sys
 import os
 from datetime import datetime
-from exception import CustomException
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
@@ -14,4 +13,3 @@ logging.basicConfig(
     format = "[%(asctime)s] - %(lineno)d - %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO
 )
-
