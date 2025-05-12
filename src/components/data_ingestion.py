bbb6518ae9d69bb9cbc51af6092bd7c3d6f46a8d
Azhar Siddiqui <azhar.sid@icloud.com>
2025-05-12 13:59:00 +0530

    Adding the Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.exception import CustomException
 from src.logger import logging
+from data_transformation import DataTransformation,DataTransformationConfig
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -38,4 +39,7 @@ class DataIngestion:
         
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
