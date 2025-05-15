7c69c14b29eed846663ebf6530cf22d6668151d9
Azhar Siddiqui <azhar.sid@icloud.com>
2025-05-15 10:58:39 +0530

    Adding Updating Utils to perform GridSearchCV

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 from exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_obejcts(file_path,obj):
     try:
@@ -19,11 +20,16 @@ def save_obejcts(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,x_test,y_train,y_test,models:dict):
+def evaluate_model(x_train,x_test,y_train,y_test,models:dict,params):
     try:
         report = {}
         for i in range(len(list(models.keys()))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            grid_search = GridSearchCV(estimator=model,param_grid=param,cv=3,scoring='r2',refit=True)
+            grid_search.fit(x_train,y_train)
+            # model.fit(x_train,y_train)
+            model.set_params(**grid_search.best_params_)
             model.fit(x_train,y_train)
             y_test_pred = model.predict(x_test)
             test_model_score = r2_score(y_test,y_test_pred)
